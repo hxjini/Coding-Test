764de826901c2b5d48fd2518859b30490a55e55d
hxjini <167085275+hxjini@users.noreply.github.com>
2024-06-15 14:49:32 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 4.47 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// arr_len은 배열 arr의 길이입니다.
+double solution(int arr[], size_t arr_len) {
+    double answer = 0;
+    int cnt = 0;
+    double sum = 0;
+    for(int i = 0; i<arr_len;i++){
+        
+        sum += arr[i];
+        cnt++;
+        answer = sum / cnt;
+    }
+    return answer;
+}
