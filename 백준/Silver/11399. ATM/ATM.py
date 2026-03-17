897b131625c140a2eda556b04485936db8addd9c
hxjini <167085275+hxjini@users.noreply.github.com>
2026-03-17 09:24:42 +0900

    [Silver IV] Title: ATM, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -5,10 +5,7 @@ n = int(input())
 times = sorted(list(map(int, input().split())))
 
 total = 0
-for i in range(n):
-    numsum = 0
-    for j in range(0, i+1):
-        numsum += times[j]
-    total += numsum
+for i in range(1, n):
+    times[i] += times[i - 1]
 
-print(total)
+print(sum(times))
